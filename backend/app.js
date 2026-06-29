d252e9b55556fe82029c5127d4d22c0b025e2311
Ananya Ahuja <ahujaananya66@gmail.com>
2026-06-29 20:34:01 +0530

    backend change

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,12 @@ const { notFound, errorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000"
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
